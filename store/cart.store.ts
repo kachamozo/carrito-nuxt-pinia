0fc9ddc8d133ed4bccc06520534e084d59ea32ec
kachamozo <cachamocito@yahoo.com>
2024-01-25 22:50:00 -0400

    add comentario

--- store/cart.store.ts
+++ store/cart.store.ts
@@ -1,20 +1,21 @@
 interface ProductProps {
-  nombre: string;
-  precio: number;
-  imagen: string;
-  cantidadTotal: string;
-  precioTotal: number;
+	nombre: string;
+	precio: number;
+	imagen: string;
+	cantidadTotal: string;
+	precioTotal: number;
 }
 interface CarritoProps {
-  productos: ProductProps[];
-  precioTotal: number;
-  cantidadTotal: number;
+	productos: ProductProps[];
+	precioTotal: number;
+	cantidadTotal: number;
 }
 
-export const cartStore = defineStore("producto", {
-  state: (): CarritoProps => ({
-    productos: [],
-    precioTotal: 0,
-    cantidadTotal: 0,
-  }),
+//usa siempre el state, get, actions
+export const cartStore = defineStore('producto', {
+	state: (): CarritoProps => ({
+		productos: [],
+		precioTotal: 0,
+		cantidadTotal: 0,
+	}),
 });
